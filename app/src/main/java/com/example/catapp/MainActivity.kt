e10502f9ba53a805340a20cd24dded8db5105aab
KONO <aleslam12345@gmail.com>
2022-08-03 01:13:17 +0200

    z to w

--- app/src/main/java/com/example/catapp/MainActivity.kt
+++ app/src/main/java/com/example/catapp/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var x:Int =5
+        var w:Int =5
     }
 }
