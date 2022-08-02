72940833021f859d7156422c117dadc82521482f
Mahmoud Elshahatt <mahmoudelshahatt1@gmail.com>
2022-08-03 00:47:36 +0200

    setting the color of the nav. bar

--- app/src/main/java/com/example/catapp/MainActivity.kt
+++ app/src/main/java/com/example/catapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.catapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.catapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
